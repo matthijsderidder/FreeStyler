4ff0fdd09861b3eda562f23fa498d4679d326c79
Matthijs de Ridder <matthijsderidder@gmail.com>
2023-02-09 18:17:15 +0100

    Extended tests

--- FreeStyler.Tests/FreeStylerTests.cs
+++ FreeStyler.Tests/FreeStylerTests.cs
@@ -22,6 +22,7 @@ namespace FreeStyler.Tests
             {
                 logging.SetMinimumLevel(LogLevel.Debug);
                 logging.AddConsole();
+                logging.AddDebug();
             });
             services.AddSingleton<FreeStylerClient>();
             _provider = services.BuildServiceProvider();
@@ -264,7 +265,7 @@ namespace FreeStyler.Tests
         [TestMethod]
         public async Task GetFixtures()
         {
-            await Connect();           
+            await Connect();
 
             _logger.LogDebug("Get fixture addresses");
             var addresses = await _client.SendRequestAsync<byte>(FreeStylerRequest.FixtureAddresses);
@@ -284,6 +285,336 @@ namespace FreeStyler.Tests
             for (var i = 0; i < addresses.Length; i++)
                 sb.AppendLine($"{(addresses[i]):D3}\t{statuses[i]}\t{names[i]}");
             _logger.LogInformation(sb.ToString().TrimEnd());
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Show Zone 1
+        /// </summary>
+        [TestMethod]
+        public async Task ShowZone1()
+        {
+            await Connect();
+
+            _logger.LogDebug("Show Zone 1");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.Zone1);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Show Zone 2
+        /// </summary>
+        [TestMethod]
+        public async Task ShowZone2()
+        {
+            await Connect();
+
+            _logger.LogDebug("Show Zone 2");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.Zone2);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Show Zone 3
+        /// </summary>
+        [TestMethod]
+        public async Task ShowZone3()
+        {
+            await Connect();
+
+            _logger.LogDebug("Show Zone 3");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.Zone3);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Show Zone 4
+        /// </summary>
+        [TestMethod]
+        public async Task ShowZone4()
+        {
+            await Connect();
+
+            _logger.LogDebug("Show Zone 4");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.Zone4);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open All Windows
+        /// </summary>
+        [TestMethod]
+        public async Task OpenAllWindows()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open All Windows");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenAll);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Gobo Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenGoboWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Gobo Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenGobo);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Color Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenColorWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Color Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenColor);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Pan/Tilt Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenPanTiltWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Pan/Tilt Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenPanTilt);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Beam Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenBeamWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Beam Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenBeam);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Macro Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenMacroWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Macro Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenMacro);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Lamp Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenLampWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Lamp Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenLamp);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Create Sequence Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenCreateSequenceWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Create Sequence Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenCreateSequence);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Cue Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenCueWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Cue Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenCue);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Sound Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenSoundWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Sound Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenSound);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Output Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenOutputWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Output Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenOutput);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Framing Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenFramingWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Framing Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenFraming);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Override Buttons Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenOverrideButtonsWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Override Buttons Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenOverrideButtons);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Command Line Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenCommandLineWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Command Line Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenCommandLine);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Master Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenMasterWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Master Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenMaster);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Submaster Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenSubmasterWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Submaster Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenSubmaster);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Smoke Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenSmokeWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Smoke Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.OpenSmoke);
+            Assert.IsTrue(result, "Button press failed!");
+
+            await Disconnect();
+        }
+
+        /// <summary>
+        /// Open Custom Sliders Window
+        /// </summary>
+        [TestMethod]
+        public async Task OpenCustomSlidersWindow()
+        {
+            await Connect();
+
+            _logger.LogDebug("Open Custom Sliders Window");
+            var result = await _client.PressButtonAsync(FreeStylerCommand.ShowSliders);
+            Assert.IsTrue(result, "Button press failed!");
 
             await Disconnect();
         }
